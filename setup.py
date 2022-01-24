3090bdc9bd627c965d1d9653071117cd2f6a633b
vinid <chiccobia@gmail.com>
2022-01-24 13:21:32 +0100

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MilaNLProc/twitter-demographer',
-    version='0.1.3',
+    version='0.1.4',
     zip_safe=False,
 )
