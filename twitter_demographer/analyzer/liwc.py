30cc914f038f9e7f8d713b0b6d71f27bf4ac790c
vinid <chiccobia@gmail.com>
2022-03-08 12:20:26 +0100

    adding support for liwc and nominatim

---
+++ twitter_demographer/analyzer/liwc.py
@@ -0,0 +1,40 @@
+from twitter_demographer.components import Component, not_null
+import re
+import liwc
+from collections import Counter
+
+def liwc_tokenize(text):
+    text = text.lower()
+    for match in re.finditer(r'\w+', text, re.UNICODE):
+        yield match.group(0)
+
+class LIWCAnalyzer(Component):
+
+    def __init__(self, liwc_dic, **kwargs):
+
+        parse, category_names = liwc.load_token_parser(liwc_dic)
+        self.category_names = []
+        self.parse = parse
+        super().__init__(**kwargs)
+
+    def outputs(self):
+        return [f"LIWC_{k}" for k in self.category_names]
+
+    def inputs(self):
+        return ["text"]
+
+    @not_null("text")
+    def infer(self, data, *args):
+
+        results = self.initialize_return_dict()
+
+        for text in data["text"].values.tolist():
+            tokens = liwc_tokenize(text)
+
+            counts = dict(Counter(category for token in tokens for category in self.parse(token)))
+
+            for item, value in counts.items():
+                results[f"LIWC_{item}"].append(value)
+
+        return results
+
---
+++ twitter_demographer/geolocation/nominatim.py
@@ -0,0 +1,41 @@
+from tqdm import tqdm
+import geocoder
+from twitter_demographer.components import Component
+import logging
+from twitter_demographer.components import not_null
+
+class NominatimDecoder(Component):
+    """
+    Wrappers on the geocoder API to disambiguate users' locations using nominatim from open street map
+    """
+
+    def __init__(self, server_url):
+        super().__init__()
+        self.server_url = server_url
+
+    def outputs(self):
+        return ["nominatim_city", "nominatim_country"]
+
+    def inputs(self):
+        return ["location"]
+
+    @not_null("location")
+    def infer(self, data):
+        logger = logging.StreamHandler()
+        logger.setLevel(logging.ERROR)
+        geo = self.initialize_return_dict()
+        pbar = tqdm(total=len(data), position=1)
+        pbar.set_description("Geocoder")
+
+        for val in data["location"]:
+            g = geocoder.osm(val, server_url=self.server_url).osm
+
+            if "addr:country" in g:
+                geo["nominatim_country"].append(g["addr:conuntry"])
+
+            if "addr:city" in g:
+                geo["nominatim_city"].append(g["addr:city"])
+
+            pbar.update(1)
+        pbar.close()
+        return geo
