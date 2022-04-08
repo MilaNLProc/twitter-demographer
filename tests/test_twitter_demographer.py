3fd49a5e617840c8d46b65c3b22a17240276d5a3
vinid <chiccobia@gmail.com>
2022-04-08 12:53:40 +0200

    adding tests

--- tests/test_twitter_demographer.py
+++ tests/test_twitter_demographer.py
@@ -3,19 +3,38 @@
 """Tests for `twitter_demographer` package."""
 
 import pytest
+import os
 from twitter_demographer import twitter_demographer
-from twitter_demographer.geolocation.geonames import GeoNamesDecoder
+from twitter_demographer.geolocation.nominatim import NominatimDecoder
 from twitter_demographer.components import Rehydrate
+import pandas as pd
+from twitter_demographer.analyzer.liwc import LIWCAnalyzer
 
 def test_demographer():
     demo = twitter_demographer.Demographer()
+    data = pd.DataFrame({"tweet_ids": ["1477976329710673921", "1467887350084689928"]})
 
-    re = Rehydrate("test")
-    geo = GeoNamesDecoder("gege")
+    re = Rehydrate(os.getenv("tweepy_bearer_token"))
+    geo = NominatimDecoder()
 
     demo.add_component(re)
     demo.add_component(geo)
 
-    # todo: need to find a way to test components, not sure github actions can support demanding computations
+    demo.infer(data)
+
+def test_liwc():
+    demo = twitter_demographer.Demographer()
+    le = LIWCAnalyzer("tests/alpha.dic")
+
+    data = pd.DataFrame(
+        {"text": ["Any alpha bravo charlie Bravo"], "screen_name": ["a"], "tweet_ids": [1], "name": ["k"],
+         "user_id_str": [12], "user_id": [12], "id": [12], "profile_image_url": ["hht"], "description": ["asdafs"]})
+
+    demo.add_component(le)
+
+    data = demo.infer(data)
+
+    assert data["LIWC_A"].values[0] == 3
+    assert data["LIWC_Bravo"].values[0] == 2
 
 
